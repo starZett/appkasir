8df498b83b601ff03637bf484ebd04333d6e17bc
starZett <assyayudah05@gmail.com>
2024-01-04 13:16:03 +0800

    Pembuatan Awal Aplikasi Kasir

---
+++ menu_manajemen.php
@@ -0,0 +1,86 @@
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-table"></i>
+        <p>
+            Data Utama
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="index.php?p=produk" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Produk</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="index.php?p=pelanggan" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Pelanggan</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="index.php?p=user" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>User</p>
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-file"></i>
+        <p>
+            Penjualan
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="index.php?p=tambah" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Tambah</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="index.php?p=histori" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Histori</p>
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item">
+    <a href="index.php?p=laporan" class="nav-link">
+        <i class="nav-icon fas fa-print"></i>
+        <p>
+            Laporan
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-cog"></i>
+        <p>
+            Maintenance
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="index.php?p=backup" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Backup</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="index.php?p=restore" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Restore</p>
+            </a>
+        </li>
+    </ul>
+</li>
